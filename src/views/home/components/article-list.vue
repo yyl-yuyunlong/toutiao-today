eb85275da8ec5fbd716d4a6eec2386d83d666bd4
18969515593 <1810369031@qq.com>
2021-05-17 21:10:59 +0800

    init

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="article-list">
+      <!--
+          List 组件通过 loading 和 finished 两个变量控制加载状态，
+          当组件初始化或滚动到到底部时，会触发 load 事件并将 loading 设置成 true，此时可以发起异步操作并更新数据，数据更新完毕后，将 loading 设置成 false 即可。
+          若数据已全部加载完毕，则直接将 finished 设置成 true 即可。
+          • load 事件：
+          • List 初始化后会触发一次 load 事件，用于加载第一屏的数据。
+          • 如果一次请求加载的数据条数较少，导致列表内容无法铺满当前屏幕，List 会继续触发 load 事件，直到内容铺满屏幕或数据全部加载完成。
+          • loading 属性：控制加载中的 loading 状态
+          • 非加载中，loading 为 false，此时会根据列表滚动位置判断是否触发 load 事件（列表内容不足一屏幕时，会直接触发）
+          • 加载中，loading 为 true，表示正在发送异步请求，此时不会触发 load 事件
+          • finished 属性：控制加载结束的状态
+          • 在每次请求完毕后，需要手动将 loading 设置为 false，表示本次加载结束
+          • 所有数据加载结束，finished 为 true，此时不会触发 load 事件
+      -->
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+        >
+        <van-cell
+         v-for="(article, index) in articles"
+         :key="index"
+         :title="article.title"
+        />
+      </van-list>
+  </div>
+</template>
+
+<script>
+import { getArticles } from '@/api/article'
+export default {
+  name: 'ArticleList',
+  components: {},
+  props: {
+    channel: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      articles: [], // 数据列表
+      loading: false, // 控制加载中的 loading 状态
+      finished: false, // 控制加载结束的状态，当加载结束，不再触发加载更多
+      timestamp: null // 获取下一页数据的时间戳
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+    async onLoad () {
+      /* console.log('onliad') */
+      // 1. 请求获取数据
+      const { data } = await getArticles({
+        channel_id: this.channel.id, // 频道ID
+        timestamp: this.timestamp || Date.now(), // 时间戳，请求新的推荐数据传当前的时间戳，
+        // 请求历史推荐传指定的时间戳，
+        // timestamp 相当于页码，请求最新数据使用当前最新时间戳，
+        // 下一页数据使用上一次返回的数据中的时间戳
+        with_top: 1 // 是否包含置顶，进入页面第一次请求时要包含置顶文章，1-包含置顶，0-不包含
+      })
+      /* console.log(data) */
+
+      // 2. 把数据放到 list 数组中
+      const { results } = data.data
+      this.articles.push(...results)
+
+      // 3. 设置本次加载状态结束，它才可以判断是否需要加载下一次，否则就会永远的停在这里
+      this.loading = false
+
+      // 4. 数据全部加载完成
+      if (results.length) {
+        // 更新获取下一页数据的页码
+        this.timestamp = data.data.pre_timestamp
+      } else {
+        // 没有数据了，把加载状态设置结束，不再触发加载更多
+        this.finished = true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  .article-list {
+      position: fixed;
+      left: 0;
+      right: 0;
+      bottom: 50px;
+      top: 90px;
+      overflow-y: auto;
+  }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,86 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="app-nav-bar">
+      <van-button
+        slot="title"
+        class="search-btn"
+        icon="search"
+        type="info"
+        round
+        size="small"
+      >搜索</van-button>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+
+    <!-- 文章频道列表 -->
+    <!--
+      标签页组件有一个功能，只有你第1次查看标签页的时候才会渲染里面的内容
+     -->
+    <van-tabs v-model="active">
+      <van-tab
+       :title="channel.name"
+       v-for="channel in channels"
+       :key="channel.id"
+      >
+        <!-- 文章列表 -->
+        <article-list :channel="channel"/>
+        <!-- /文章列表 -->
+      </van-tab>
+    </van-tabs>
+    <!-- /文章频道列表 -->
+
+  </div>
 </template>
 
 <script>
+import { getUserChannels } from '@/api/user'
+import ArticleList from './components/article-list.vue'
 export default {
   name: 'HomeIndex',
-  components: {},
+  components: {
+    ArticleList
+  },
   props: {},
   data () {
-    return {}
+    return {
+      active: 0, // 控制被激活的标签
+      channels: [] // 频道列表
+    }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    async loadChannels () {
+      // 请求获取频道数据
+      const { data } = await getUserChannels()
+      this.channels = data.data.channels
+    }
+  }
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+  .home-container {
+    /deep/ .van-nav-bar__title{
+      max-width: none;
+      .search-btn {
+       width: 277px;
+       height: 32px;
+       background-color: #5babfb;
+       border: none;
+       .van-icon {
+         font-size: 16px;
+       }
+       .van-button__text {
+         font-size: 14px;
+       }
+     }
+    }
+  }
+
+</style>
