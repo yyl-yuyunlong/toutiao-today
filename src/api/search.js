0a6a95bcc2ec2b59b1f1fcbdb7fff712ea97efce
18969515593 <1810369031@qq.com>
2021-05-24 10:37:39 +0800

    init

---
+++ src/api/search.js
@@ -0,0 +1,29 @@
+/**
+ * 搜索相关请求模块
+ */
+
+import request from '@/utils/request'
+
+/**
+  * 获取搜索联想建议
+  */
+export const getSearchSuggestions = q => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/suggestion',
+    params: {
+      q // 联想建议关键词
+    }
+  })
+}
+
+/**
+  * 获取搜索结果
+  */
+export const getSearchResult = params => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/search',
+    params
+  })
+}
--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="search-suggestion">
-    <van-cell title="黑马程序员..." icon="search"></van-cell>
+    <van-cell
+     :title="str"
+     icon="search"
+     v-for="(str, index) in suggestions"
+     :key="index"
+    >
+      <div slot="title" v-html="highlight(str)"></div>
+    </van-cell>
   </div>
 </template>
 
 <script>
+
+import { getSearchSuggestions } from '@/api/search'
+import { debounce } from 'lodash'
+
 export default {
   name: 'SearchSuggestion',
   components: {},
@@ -15,25 +26,35 @@ export default {
     }
   },
   data () {
-    return {}
+    return {
+      suggestions: [] // 联想建议数据列表
+    }
   },
   computed: {},
   watch: {
     // 属性名： 要监视的数据名称
     searchText: {
-      handler () {
+      handler: debounce(async function () {
         // 找到数据接口
         // 请求获取数据
         // 模板绑定展示
-        console.log('object')
-      },
+        const { data } = await getSearchSuggestions(this.searchText)
+        this.suggestions = data.data.options
+      }, 200),
       // 用于设置回调在真挺开始之后立刻调用
       immediate: true
     }
   },
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    highlight (str) {
+      // 正则表达式中 /中间的内容/ 都会被当作字符串来对待
+      /* str.replace(正则表达式, `<span style ="color:red">${this.searchText}</span>`) */
+      // RegExp 是正则表达式的构造函数，参数1 :字符串；参数2 : 匹配模式；返回值：正则对象
+      return str.replace(new RegExp(this.searchText, 'gi'), `<span style ="color:red">${this.searchText}</span>`)
+    }
+  }
 }
 </script>
 
